07f3655283d17349932a2e2b9f7884d764be31d0
hiralshah7 <90278193+hiralshah7@users.noreply.github.com>
2022-11-30 20:46:43 -0500

    hamburger_fix

--- js/components/Thelightboxcomponent.js
+++ js/components/Thelightboxcomponent.js
@@ -6,19 +6,20 @@ export default {
     template: `
     <div class="lightbox">
     <div class="content">
-    <div class="closeicon">
-    <ion-icon @click="closeLB" name="close-circle-sharp"></ion-icon>
-</div>
-        <h2 class="name">{{ hero.name }}</h2>
+        <div class="closeicon">
+      
+            <ion-icon @click="closeLB" name="close-circle-sharp"></ion-icon>
+            <h2 class="name">{{ hero.name }}</h2>
+        
+        </div>
         <div class="content">
             <h3 class="tech">Technology Used:</h3>
-            <p>{{ hero.technology_used }}<p>
+            <p>{{ hero.technology_used }}</p>
             <h3 class="description1">Description</h3>
             <p class="desc">{{ hero.Description}}</p>
         </div>
     </div>
 </div>
-    </div>
   `,
     methods: {
         closeLB() {
--- js/main.js
+++ js/main.js
@@ -4,7 +4,37 @@ import LightBox from './components/Thelightboxcomponent.js';
 
 (() => {
 
-    // creating the vue app
+    //Toggle menu for the hamburguer menu
+    const header = document.querySelector("header");
+
+    window.addEventListener("scroll", function() {
+        header.classList.toggle("sticky", window.scrollY > 0);
+    });
+
+    let menu = document.querySelector('#menu-icons');
+    let navlist = document.querySelector('.navlist');
+
+    menu.onclick = () => {
+        menu.classList.toggle('bx-x');
+        navlist.classList.toggle('active');
+    };
+
+    window.onscroll = () => {
+        menu.classList.remove('bx-x');
+        navlist.classList.remove('active');
+    };
+
+    const sr = ScrollReveal({
+        distance: '45px',
+        duration: 2400,
+        reset: true
+    })
+
+    sr.reveal('.home-text', { delay: 350, origin: 'left' })
+    sr.reveal('.home-img', { delay: 350, origin: 'right' })
+
+    sr.reveal('.sub-service,.about,.main,.cta', { delay: 100, origin: 'bottom' })
+        // creating the vue app
     const { createApp } = Vue
 
     createApp({
